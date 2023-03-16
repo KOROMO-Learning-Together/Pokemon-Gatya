6cd24d1ff8e4d880c0daf9a7999dbbd5fb38a27b
Gaterise <anexaki@gmail.com>
2023-03-16 21:38:03 +0900

    jsdomを追加してレンダリングのテストサンプルを書いた

--- index.test.js
+++ index.test.js
@@ -1,6 +1,29 @@
-jest.mock("./index.html");
+const fs = require("fs");
+const { JSDOM } = require("jsdom");
 
-test("test", () => {
-  const test = jest.document.getElementById("root");
-  expect(test).toBeInTheDocument();
+describe("test", () => {
+  let document;
+  beforeEach(() => {
+    //index.htmlをテキストとして読み込む
+    const indexHtmlText = fs.readFileSync("./index.html", {
+      encoding: "utf-8",
+    });
+    //テキストを仮想DOMにする
+    const jsdom = new JSDOM(indexHtmlText).window;
+    document = jsdom.document;
+  });
+
+  it("rootというidを持った要素があること", () => {
+    const rootElement = document.getElementById("root");
+
+    expect(rootElement.innerHTML).toBe("test!!!!");
+  });
+
+  it("root要素のテキストをmasashiに変えていること", () => {
+    const rootElement = document.getElementById("root");
+
+    rootElement.innerHTML = "masashi";
+
+    expect(rootElement.innerHTML).toBe("masashi");
+  });
 });
