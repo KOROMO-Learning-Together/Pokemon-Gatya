856a5ce141acc601a57dfc4c425ab4464c92bebb
Kazunori Nakajima <kazunori_nakajima_aa@mail.toyota.co.jp>
2023-04-19 16:49:08 +0900

    day3_start作成完了

--- public/PokemonGacha.js
+++ public/PokemonGacha.js
@@ -1,6 +1,6 @@
 export class PokemonGacha {
     constructor(mode){
-        mode = mode;
+        this.mode = mode;
     }
 
     pokemon = {}
@@ -11,11 +11,14 @@ export class PokemonGacha {
     async start(){
         // 1-❶
         await this.randomSelectPokemon()
+        // 3-❺
+        // this.randomSelectMonsterBall()
         // 1-❻
         const mainContainer = document.getElementById('mainContainer');
         mainContainer.remove()
-        // 1-❼
+        // 1-❼ → 3-❶
         await this.showPokemon()
+        // await this.gachaAction()
     }
 
     // ボタンにカーソルが乗ったときに実行されるメソッド
@@ -53,7 +56,7 @@ export class PokemonGacha {
         const pageElement = document.getElementById("page")
         pageElement.appendChild(video);
 
-        // 動画再生終了時のアクション
+        // 再生終了時のアクション
 	    video.addEventListener("ended", async () => {
             // 表示している動画のフレームを削除
             const gacha = document.getElementById('gacha');
@@ -65,16 +68,33 @@ export class PokemonGacha {
 
     // ゲットしたポケモンを表示するメソッド
     async showPokemon(){
+        // ポケモンの画像を取得
         // 1-❽
         const pokemonImg = await this.makePokemonImg()
+
+        // ポケモンのステータス表示を作成
+        // 2-❶
+        const pokemonStatus = await this.makePokemonStatus()
+
+        // コンティニューボタンを作成
+        // 3-❷
+        // const continueButton = this.makeContinueButton()
+
+        // 画面を作成
         // 1-❾
         const pokemonWindow = document.createElement('div');
-        pokemonWindow.id = 'pokemonWindow';
-        pokemonWindow.appendChild(pokemonImg)
-        const page = document.getElementById('page')
-        page.appendChild(pokemonWindow)
-    }
+        pokemonWindow.id = 'pokemonWindow'
+        pokemonWindow.appendChild(pokemonImg);
+        // 2-❷
+        pokemonWindow.appendChild(pokemonStatus); 
+        // コンティニューボタンを表示
+        // 3-❸
+        // pokemonWindow.appendChild(continueButton); 
 
+        const pageElement = document.getElementById("page")
+        pageElement.appendChild(pokemonWindow);
+
+    }
     // ポケモンの画像を取得した情報から作成するためのメソッド
     async makePokemonImg(){
         // ポケモンの画像を取得する
@@ -132,27 +152,286 @@ export class PokemonGacha {
         return header
     }
 
-    // ポケモンのステータスを取得した情報から作成するためのメソッド
     async makePokemonStatus(){
+        const statusL = document.createElement('div');
+        statusL.id =  'statusL'
+
+        //　2-❹
+        const height = document.createElement('p');
+        height.id = 'height'
+        height.innerHTML = `身長：${this.pokemon.height*10} cm`
+        //　2-❺
+        const weight = document.createElement('p');
+        weight.id = 'weight'
+        weight.innerHTML = `体重：${this.pokemon.weight/10} kg`
+
+        const genera = document.createElement('p');
+        genera.id = 'genera'
+        genera.innerHTML = `分類：${this.species.genera.filter((element)=>element.language.name === 'ja')[0].genus}`
+
+        const types = document.createElement('p');
+        types.id = 'types';
+
+        let pokemonTypes = '';
+
+        this.pokemon.types.forEach(async element => {
+            const res = await fetch(element.type.url)
+            const type = await res.json();
+            pokemonTypes += type.names.filter(element => element.language.name === 'ja')[0].name
+            pokemonTypes += '/'
+            types.innerHTML = `タイプ：${pokemonTypes}`
+        });
+
+        const abilities = document.createElement('p');
+        abilities.id = 'abilities';
+
+        let pokemonAbilities = '';
+
+        this.pokemon.abilities.forEach(async element => {
+            const res = await fetch(element.ability.url)
+            const ability = await res.json();
+            pokemonAbilities += ability.names.filter(element => element.language.name === 'ja')[0].name
+            pokemonAbilities += '/'
+            abilities.innerHTML = `特性：${pokemonAbilities}`
+        });
+
+
+        statusL.appendChild(genera)
+        statusL.appendChild(types)
+        statusL.appendChild(height)
+        statusL.appendChild(weight)
+        statusL.appendChild(abilities)
+
+        const statusR = document.createElement('div');
+        statusR.id =  'statusR'
+
+        const hpColumn = document.createElement('div');
+        hpColumn.id =  'hpColumn'
+        const hp = document.createElement('div');
+        hp.id =  'hp'
+        hp.innerHTML = 'HP  '
+        const hpGage = document.createElement('div');
+        hpGage.id =  'hpGage'
+        let hpCount = Math.floor(this.pokemon.stats.filter(element => element.stat.name === 'hp')[0].base_stat/10)
+        if(hpCount>15){hpCount = 15}
+        for (let i = 0; i < hpCount; i++){
+            const yellowGage = document.createElement('div');
+            yellowGage.className = 'yellowGage'
+            hpGage.appendChild(yellowGage)
+        }
+        for (let i = 0; i < 15-hpCount; i++){
+            const whiteGage = document.createElement('div');
+            whiteGage.className = 'whiteGage'
+            hpGage.appendChild(whiteGage)
+        }
+
+        hpColumn.appendChild(hp)
+        hpColumn.appendChild(hpGage)
+
+        const attackColumn = document.createElement('div');
+        attackColumn.id =  'attackColumn'
+        const attack = document.createElement('div');
+        attack.id =  'attack'
+        attack.innerHTML = 'こうげき'
+        const attackGage = document.createElement('div');
+        attackGage.id =  'attackGage'
+        let attackCount = Math.floor(this.pokemon.stats.filter(element => element.stat.name === 'attack')[0].base_stat/10)
+        if(attackCount>15){hpCount = 15}
+        for (let i = 0; i < attackCount; i++){
+            const yellowGage = document.createElement('div');
+            yellowGage.className = 'yellowGage'
+            attackGage.appendChild(yellowGage)
+        }
+        for (let i = 0; i < 15-attackCount; i++){
+            const whiteGage = document.createElement('div');
+            whiteGage.className = 'whiteGage'
+            attackGage.appendChild(whiteGage)
+        }
+
+        attackColumn.appendChild(attack)
+        attackColumn.appendChild(attackGage)
+
+        const defenseColumn = document.createElement('div');
+        defenseColumn.id =  'defenseColumn'
+        const defense = document.createElement('div');
+        defense.id =  'defense'
+        defense.innerHTML = 'ぼうぎょ'
+        const defenseGage = document.createElement('div');
+        defenseGage.id =  'defenseGage'
+        let defenseCount = Math.floor(this.pokemon.stats.filter(element => element.stat.name === 'defense')[0].base_stat/10)
+        if(defenseCount>15){defenseCount = 15}
+        for (let i = 0; i < defenseCount; i++){
+            const yellowGage = document.createElement('div');
+            yellowGage.className = 'yellowGage'
+            defenseGage.appendChild(yellowGage)
+        }
+        for (let i = 0; i < 15-defenseCount; i++){
+            const whiteGage = document.createElement('div');
+            whiteGage.className = 'whiteGage'
+            defenseGage.appendChild(whiteGage)
+        }
+
+        defenseColumn.appendChild(defense)
+        defenseColumn.appendChild(defenseGage)
+
+
+
+        const spAttackColumn = document.createElement('div');
+        spAttackColumn.id =  'spAttackColumn'
+        const spAttack = document.createElement('div');
+        spAttack.id =  'spAttack'
+        spAttack.innerHTML = 'とくこう'
+        const spAttackGage = document.createElement('div');
+        spAttackGage.id =  'spAttackGage'
+        let spAttackCount = Math.floor(this.pokemon.stats.filter(element => element.stat.name === 'special-attack')[0].base_stat/10)
+        if(spAttackCount>15){spAttackCount = 15}
+        for (let i = 0; i < spAttackCount; i++){
+            const yellowGage = document.createElement('div');
+            yellowGage.className = 'yellowGage'
+            spAttackGage.appendChild(yellowGage)
+        }
+        for (let i = 0; i < 15-spAttackCount; i++){
+            const whiteGage = document.createElement('div');
+            whiteGage.className = 'whiteGage'
+            spAttackGage.appendChild(whiteGage)
+        }
+
+        spAttackColumn.appendChild(spAttack)
+        spAttackColumn.appendChild(spAttackGage)
+
+        const spDefenseColumn = document.createElement('div');
+        spDefenseColumn.id =  'spDefenseColumn'
+        const spDefense = document.createElement('div');
+        spDefense.id =  'spDefense'
+        spDefense.innerHTML = 'とくぼう'
+        const spDefenseGage = document.createElement('div');
+        spDefenseGage.id =  'spDefenseGage'
+        let spDefenseCount = Math.floor(this.pokemon.stats.filter(element => element.stat.name === 'special-defense')[0].base_stat/10)
+        if(spDefenseCount>15){spDefenseCount = 15}
+        for (let i = 0; i < defenseCount; i++){
+            const yellowGage = document.createElement('div');
+            yellowGage.className = 'yellowGage'
+            spDefenseGage.appendChild(yellowGage)
+        }
+        for (let i = 0; i < 15-defenseCount; i++){
+            const whiteGage = document.createElement('div');
+            whiteGage.className = 'whiteGage'
+            spDefenseGage.appendChild(whiteGage)
+        }
+
+        spDefenseColumn.appendChild(spDefense)
+        spDefenseColumn.appendChild(spDefenseGage)
+
+        const speedColumn = document.createElement('div');
+        speedColumn.id =  'defenseColumn'
+        const speed = document.createElement('div');
+        speed.id =  'spDefense'
+        speed.innerHTML = 'すばやさ'
+        const speedGage = document.createElement('div');
+        speedGage.id =  'speedGage'
+        let speedCount = Math.floor(this.pokemon.stats.filter(element => element.stat.name === 'speed')[0].base_stat/10)
+        if(speedCount>15){speedCount = 15}
+        for (let i = 0; i < speedCount; i++){
+            const yellowGage = document.createElement('div');
+            yellowGage.className = 'yellowGage'
+            speedGage.appendChild(yellowGage)
+        }
+        for (let i = 0; i < 15-speedCount; i++){
+            const whiteGage = document.createElement('div');
+            whiteGage.className = 'whiteGage'
+            speedGage.appendChild(whiteGage)
+        }
+
+        speedColumn.appendChild(speed)
+        speedColumn.appendChild(speedGage)
+
+
+        statusR.appendChild(hpColumn)
+        statusR.appendChild(attackColumn)
+        statusR.appendChild(defenseColumn)
+        statusR.appendChild(spAttackColumn)
+        statusR.appendChild(spDefenseColumn)
+        statusR.appendChild(speedColumn)
+
+        const status = document.createElement('div');
+        status.id =  'status'
+
+        status.appendChild(statusL)
+        status.appendChild(statusR)
+
+
+        const flavorText =  document.createElement('p')
+        flavorText.id = 'flavorText'
+        flavorText.innerHTML = this.species.flavor_text_entries.filter(element => element.language.name === 'ja')[0].flavor_text
+
         
+
+        const description = document.createElement('div');
+        description.id =  'description'
+
+        description.appendChild(flavorText)
+
+        //　2-❸
+        const statusWindow = document.createElement('div');
+        statusWindow.id =  'statusWindow'
+
+        statusWindow.appendChild(status)
+        statusWindow.appendChild(description)
+
+        //　2-❸
+        return statusWindow
     }
 
-    // ランダムなポケモンの情報を外部APIから取得するためのメソッド
     async randomSelectPokemon(){
-        // 1-❷　1-❸
+        // ランダムなポケモンIDを生成
         const pokemonId = Math.floor(Math.random()*1000)
-        // 1-❹
+        // pokeAPIからポケモンの情報を取得
         const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/`)
-        // 1-❺
-        this.pokemon = await res.json()
+        // プロパティに格納
+        this.pokemon= await res.json()
     }
 
-    // ランダムに動画で表示するモンスターボールを選択するためのメソッド
     randomSelectMonsterBall(){
-        
+        // 設定によって出現するモンスターボールの確率を変更
+        if(this.mode === 'easy'){
+            const probability = Math.floor(Math.random()*100)
+            // 3-❼
+            // if(probability>90){
+            //     this.monsterBall = 'masterBall'
+            // } else if (probability>70){
+            //     this.monsterBall = 'hyperBall'
+            // } else if (probability>40){
+            //     this.monsterBall = 'superBall'
+            // } else {
+            //     this.monsterBall = 'normalBall'
+            // }
+        }
+    }
+    // コンティニューボタンを作るメソッド
+    makeContinueButton(){
+        // コンティニューボタンのエレメントを作成
+        // 3-❹
+        // const continueButton = document.createElement('button');
+        // continueButton.id = 'continueButton'
+        // クリック時の動作を設定
+        // 3-❺
+        // continueButton.onclick = ()=>{this.continue(this)}
+        // ボタンに挿入するイメージの作成
+        const buttonImg = document.createElement('img');
+        buttonImg.id = 'continueButtonImg'
+        buttonImg.src = 'continueButton.svg'
+        // ボタンホバー時の動作を設定
+        buttonImg.onmouseenter = ()=>{buttonImg.src = 'continueButtonHover.svg'}
+        buttonImg.onmouseout = ()=>{buttonImg.src = 'continueButton.svg'}
+        continueButton.appendChild(buttonImg)
+        // ボタンを配置するエリアを作成
+        const footer = document.createElement('div')
+        footer.id = 'footer'
+        // 作成したボタンを追加
+        footer.appendChild(continueButton)
+        return footer
     }
 
-    
     continue(gacha){
         // getしたポケモンを消す
         const pokemonWindow = document.getElementById('pokemonWindow')
--- public/index.test.js
+++ public/index.test.js
@@ -3,6 +3,10 @@ import {PokemonGacha} from './PokemonGacha.js';
 import { JSDOM } from "jsdom";
 import { jest } from '@jest/globals';
 import pikachu from './pikachu.json'
+import pikacuSpecies from './pikachuSpecies.json'
+import pikacuType from './pikachuType.json'
+import pikacuAbility1 from './pikachuAbility1.json'
+import pikacuAbility2 from './pikachuAbility2.json'
 
 describe("ポケモンガチャ全体のテスト", () => {
   let jsdom;
@@ -124,11 +128,16 @@ describe("ポケモンガチャ全体のテスト", () => {
   // day1 ここからやっていきます。
   describe("PokemonGacha.jsについてのテスト", () => {
     describe("startメソッドを実行すると", () => {
+      beforeEach(()=>{
+        gacha.randomSelectPokemon = jest.fn().mockResolvedValue('hogePokemon')
+        gacha.showPokemon = jest.fn()
+        gacha.gachaAction = jest.fn()
+        gacha.makeContinueButton = jest.fn()
+        gacha.randomSelectMonsterBall = jest.fn()
+      })
       // 1-❶
       it("randomSelectPokemonを呼び出す", async () => {
         // arange
-        gacha.randomSelectPokemon = jest.fn()
-        gacha.showPokemon = jest.fn()
 
         // act
         await gacha.start()
@@ -139,10 +148,8 @@ describe("ポケモンガチャ全体のテスト", () => {
       // 1-❻
       it("mainContainerの要素を全て消す", async () => {
         // arange
-        gacha.randomSelectPokemon = jest.fn()
         const mainContainer = document.getElementById('mainContainer');
         mainContainer.remove = jest.fn()
-        gacha.showPokemon = jest.fn()
 
         // act
         await gacha.start()
@@ -150,17 +157,26 @@ describe("ポケモンガチャ全体のテスト", () => {
         // assert
         expect(mainContainer.remove).toHaveBeenCalled()
       });
+      // 3-❻
+        it("randomSelectMonsterBallを呼び出す", async () => {
+          // arange
+  
+          // act
+          await gacha.start()
+  
+          // assert
+          expect(gacha.randomSelectMonsterBall).toHaveBeenCalled()
+        });
       // 1-❼ ⇨ 3-❶
-      it("showPokemonを呼び出す", async () => {
+      // it("showPokemonを呼び出す", async () => {
+      it("gachaActionを呼び出す", async () => {
         // arange
-        gacha.randomSelectPokemon = jest.fn().mockResolvedValue('hogePokemon')
-        gacha.showPokemon = jest.fn()
 
         // act
         await gacha.start()
 
         // assert
-        expect(gacha.showPokemon).toHaveBeenCalled()
+        expect(gacha.gachaAction).toHaveBeenCalled()
       });
     });
     describe("randomSelectPokemonを実行すると", () => {
@@ -220,6 +236,9 @@ describe("ポケモンガチャ全体のテスト", () => {
         const hogePokemonStatus = document.createElement('div')
         hogePokemonStatus.id = 'hogePokemonStatus'
         gacha.makePokemonStatus = jest.fn().mockResolvedValue(hogePokemonStatus)
+        const hogeContinueButton = document.createElement('div')
+        hogeContinueButton.id = 'hogeContinueButton'
+        gacha.makeContinueButton = jest.fn().mockReturnValue(hogeContinueButton)
       })
       // 1-❽
       it("ポケモンの画像を作成する", async () => {
@@ -244,12 +263,11 @@ describe("ポケモンガチャ全体のテスト", () => {
         const result = document.getElementById('hogePokemonImg')
         expect(page.childElementCount).toBe(1);
         expect(page.firstElementChild).toBe(pokemonWindow);
-        expect(pokemonWindow.childElementCount).toBe(1);
+        expect(pokemonWindow.childElementCount).toBe(2);
+        // コンティニューボタンを実装したらテストを書き換える
+        // expect(pokemonWindow.childElementCount).toBe(3);
         expect(pokemonWindow.firstElementChild).toBe(result);
       });
-
-      // ここからDay2
-      // pokemonのステータスを表示したい。。。
 
       // 2-❶
       it("ポケモンのステータスを作成する", async () => {
@@ -272,7 +290,33 @@ describe("ポケモンガチャ全体のテスト", () => {
         const pokemonWindow = document.getElementById('pokemonWindow')
         const result = document.getElementById('hogePokemonStatus')
         expect(pokemonWindow.childElementCount).toBe(2);
+        // コンティニューボタンを実装したらテストを書き換える
+        // expect(pokemonWindow.childElementCount).toBe(3);
         expect(pokemonWindow.children[1]).toBe(result);
+      });
+      // 3-❷
+      it("makeContinueButtonを呼び出す", async () => {
+        // arange
+
+        // act
+        await gacha.showPokemon()
+
+        // assert
+        expect(gacha.makeContinueButton).toHaveBeenCalled()
+        
+      });
+      // 3-❸
+      it("コンティニューボタンを表示する", async () => {
+        // arange
+
+        // act
+        await gacha.showPokemon()
+
+        // assert
+        const pokemonWindow = document.getElementById('pokemonWindow')
+        const result = document.getElementById('hogeContinueButton')
+        expect(pokemonWindow.childElementCount).toBe(3);
+        expect(pokemonWindow.lastElementChild).toBe(result);
       });
     });
     describe("makePokemonStatusメソッドを実行すると", () => {
@@ -280,6 +324,11 @@ describe("ポケモンガチャ全体のテスト", () => {
       beforeEach(async ()=>{
         // ピカチュウのデータをpokemonに格納しておく
         gacha.pokemon = pikachu
+        gacha.species = pikacuSpecies
+        fetch = jest.fn()
+          .mockResolvedValueOnce({json:jest.fn().mockResolvedValue(pikacuType)})
+          .mockResolvedValueOnce({json:jest.fn().mockResolvedValue(pikacuAbility1)})
+          .mockResolvedValueOnce({json:jest.fn().mockResolvedValue(pikacuAbility2)})
       })
       
       // 2-❸
@@ -287,10 +336,10 @@ describe("ポケモンガチャ全体のテスト", () => {
         // arange
 
         // act
-        const status = await gacha.makePokemonStatus()
+        const statusWindow = await gacha.makePokemonStatus()
 
         // assert
-        expect(status.id).toBe('status')
+        expect(statusWindow.id).toBe('statusWindow')
       });
 
       // 2-❹
@@ -298,12 +347,13 @@ describe("ポケモンガチャ全体のテスト", () => {
         // arange
 
         // act
-        const status = await gacha.makePokemonStatus()
-        const height = status.children[0]
+        const statusWindow = await gacha.makePokemonStatus()
+
+        const height = statusWindow.children[0].children[0].children[2]
 
         // assert
         expect(height.id).toBe('height')
-        expect(height.innerHTML).toBe('身長：40cm')
+        expect(height.innerHTML).toBe('身長：40 cm')
       });
       
       // 2-❺
@@ -311,12 +361,56 @@ describe("ポケモンガチャ全体のテスト", () => {
         // arange
 
         // act
-        const status = await gacha.makePokemonStatus()
-        const weight = status.children[1]
+        const statusWindow = await gacha.makePokemonStatus()
+        const weight = statusWindow.children[0].children[0].children[3]
 
         // assert
         expect(weight.id).toBe('weight')
-        expect(weight.innerHTML).toBe('体重：6kg')
+        expect(weight.innerHTML).toBe('体重：6 kg')
+      });
+    });
+
+    describe("makeContinueButtonメソッドを実行すると", () => {
+
+      beforeEach(async ()=>{
+        
+      })
+      
+      // 3-❹
+      it("コンティニューボタンが作成される", async () => {
+        // arange
+
+        // act
+        const continueButton = await gacha.makeContinueButton()
+
+        // assert
+        expect(continueButton.childElementCount).toBe(1)
+        expect(continueButton.firstElementChild.id).toBe('continueButton')
+      });
+
+      // 3-❺
+      it("コンティニューボタンを押すとcontinueメソッドが実行される", async () => {
+        // arange
+
+        // act
+        const continueButton = await gacha.makeContinueButton()
+
+        // assert
+        
+      });
+    });
+    describe("randomSelectMonsterBallメソッドを実行すると", () => {
+      // 3-❼
+      it("this.monsterBallに選択されたモンスターボールが保存されている", async () => {
+        // arange
+        Math.random = jest.fn().mockReturnValue(1)
+        Math.floor = jest.fn().mockReturnValue(100)
+
+        // act
+        const monsterBall = gacha.randomSelectMonsterBall()
+ 
+        // assert
+        expect(gacha.monsterBall).toBe('masterBall')
       });
     });
   });
