f7d5caaae9bc59d184ded96b7addf3649f7eace2
Kazunori Nakajima <kazunori_nakajima_aa@mail.toyota.co.jp>
2023-04-24 17:37:05 +0900

    3-5のテスト修正

--- public/index.test.js
+++ public/index.test.js
@@ -382,11 +382,15 @@ describe("ポケモンガチャ全体のテスト", () => {
       // 3-❺
       it("コンティニューボタンを押すとcontinueメソッドが実行される", async () => {
         // arange
+        gacha.continue = jest.fn()
 
         // act
         const continueButton = await gacha.makeContinueButton()
+        const click = new jsdom.MouseEvent('click')
+        continueButton.firstElementChild.dispatchEvent(click)
 
         // assert
+        expect(gacha.continue).toHaveBeenCalledWith(gacha)
 
       });
     });
